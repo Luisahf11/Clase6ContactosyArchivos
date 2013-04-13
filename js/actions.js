43f7f142dae9601fb17f060623e3418acefcb33c
Luis Alberto Hernandez <alberpapi_11062009@hotmail.com>
2013-04-13 13:06:49 -0500

    0.0.11

--- js/actions.js
+++ js/actions.js
@@ -91,5 +91,11 @@ $(document).ready(function(e) {
 					alert(err.code);
 				});
 			});
+			//Navegador en la app
+			var ref = window.open('http://apache.org', '_blank', 'location=yes');
+         ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
+         ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
+         ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
+         ref.addEventListener('exit', function(event) { alert(event.type); });			
 	}, false);
 });
